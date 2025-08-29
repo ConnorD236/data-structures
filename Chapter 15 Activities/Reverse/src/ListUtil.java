2576cbe82769b629ae0e9bc7b8173d96896a5b81
ConnorD236 <cjdoyle@stu.naperville203.org>
2025-08-29 12:51:51 -0500

    Finished Reverse LinkedList

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -13,13 +13,18 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ListIterator<String> iterator = strings.listIterator();
-        LinkedList<String> newlist = new LinkedList<>(); ListIterator<String> iterator2 = newlist.listIterator();
-        while(iterator.hasNext()) {iterator.next();}
-        while(iterator.hasPrevious()) {
-            newlist.add(iterator.previous());
+        ListIterator<String> iterator = strings.listIterator(); //iterator
+        while(iterator.hasNext()) {iterator.next();} //set iterator to right end
+
+        for(int i=1; i<=strings.size(); i++) //for up to the length of the linked list...
+        {
+            String string = "anti-error statement";
+            for(int l=i; l>0; l--) {string = iterator.previous();} //...move the iterator left that many times
+            iterator.remove(); //remove string (returned by iterator.previous())
+
+            while(iterator.hasNext()) {iterator.next();} //reset to right end
+            iterator.add(string); //add string back at the end
+            //and repeat, going farther back in the list
         }
-        strings = newlist;
-        
     }
 }

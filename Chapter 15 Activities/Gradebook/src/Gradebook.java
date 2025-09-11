65aad9ea43181546de5273614891b9e8e0bf25f7
ConnorD236 <cjdoyle@stu.naperville203.org>
2025-09-11 13:10:12 -0500

    Gradebook

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-. . .
+import java.util.*;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,7 +10,7 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> gradebook = new HashMap<>();
 
         boolean done = false;
         while(!done)
@@ -22,17 +22,35 @@ public class Gradebook
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("Who would you like to add?");
+                String name = in.next();
+                System.out.println("What is their course grade?");
+                String grade = in.next();
+                gradebook.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Who would you like to remove?");
+                String name = in.next();
+                gradebook.remove(name);
+                
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("Who would you like to modify?");
+                String name = in.next();
+                System.out.println("What is their course grade?");
+                String grade = in.next();
+                gradebook.put(name, grade);
+                
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Map<String, String> sortedGradebook = new TreeMap<>(gradebook);
+                Set<String> keys = sortedGradebook.keySet();
+                for(String s: keys) {
+                    System.out.println(s+" "+gradebook.get(s));
+                }
+                
+                
             } else
             {
                 done = true;

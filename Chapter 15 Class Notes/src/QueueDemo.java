268777e1ba5fc7533310582906ce2f584dd04ca7
ConnorD236 <cjdoyle@stu.naperville203.org>
2025-09-16 13:20:37 -0500

    Queue and PriorityQueue

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,33 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+        jobs.add("Joe: Quarter 2 Expense Report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        // Add some more print jobs
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        jobs.add("Katherine: Can I Get Fired For This");
+
+        System.out.println("Printing: "+jobs.remove());
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Boss: Katherine Termination Letter");
+        
+        //Print the rest of the jobs in the queue
+        /* int size = jobs.size();
+        for(int i=0; i<size; i++) {
+            System.out.println("Printing: "+jobs.remove());
+        } */
+       for(String s: jobs)
+
+        while(jobs.size() > 0) {
+            System.out.println("Printing: "+jobs.remove());
+        }
     }
 }
